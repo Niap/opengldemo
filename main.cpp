0bbe179a908fa069a81613b1e24c9645fb8014af
visla-zhupeiran <peiran.zhu@visla.us>
2021-08-31 09:48:27 +0800

    work with macOS

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #define ASSET(x) if(!x) __debugbreak();
 #define GLCALL(x) GlClearError(); \
     x\
-    ASSET(GlGetError(__LINE__))
+    GlGetError(__LINE__);
 
 static void GlClearError(){
     while(GL_NO_ERROR != glGetError());
@@ -73,6 +73,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
